445ca0393f99823434884d16593355b99ecb0030
shashwat kapoor <147691001+shashwat-kapoor@users.noreply.github.com>
2024-08-02 13:40:17 +0530

    Unity Setup

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Range(1, 10)]
+    [SerializeField] private float speed = 10f;
+
+    [Range(1, 10)]
+    [SerializeField] private float lifetime = 3f;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, lifetime);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+
+    }
+
+
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+
+    //gun var 
+    [SerializeField] private GameObject BulletPrefab;
+    [SerializeField] private Transform FiringPoint;
+    [Range(0.1f, 1f)]
+    [SerializeField] private float fireRate = 0.5f;
+
+    private Rigidbody2D rb;
+    private float mx;
+    private float my;
+
+    private float fireTimer;
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        my = Input.GetAxisRaw("Vertical");
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        if (Input.GetMouseButton(0) && fireTimer <= 0f)
+        {
+            Shoot();
+            fireTimer = fireRate;
+
+        }
+        else
+        {
+            fireTimer -= Time.deltaTime;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(mx, my).normalized * speed; 
+    }
+
+    private void Shoot()
+    {
+        Instantiate(BulletPrefab, FiringPoint.position, FiringPoint.rotation);
+    }
+}
